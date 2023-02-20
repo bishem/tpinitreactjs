45965108c17b9fd50017249c6c2ba0c113ee4c64
Cheshire <aminrsaidali@gmail.com>
2023-02-20 22:56:31 +0100

    added page structure
    header and footer

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Logo.css";
+
+const AUTHOR = 'Amin MOHAMED';
+
+const Logo = () => {
+  return (
+    <div className="logo flex center">{AUTHOR}</div>
+  );
+}
+
+export default Logo;
